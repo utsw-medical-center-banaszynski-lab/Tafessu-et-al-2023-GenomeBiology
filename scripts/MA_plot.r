24758c85c60b0f1ad6e1a3960646afb5b6321455
AmanuelTafessu <49997327+AmanuelTafessu@users.noreply.github.com>
2021-11-05 16:39:59 -0500

    Update MA_plot.r

--- scripts/MA_plot.r
+++ scripts/MA_plot.r
@@ -11,7 +11,7 @@ source_gist("524eade46135f6348140")
 library(ggpubr)
 library(ggrepel)
 
-TF_list <- read.table(file = "TF_cofactor_baseMean_FC_label.xls", header= T, sep="\t",stringsAsFactors=FALSE)
+TF_list <- read.table(file = "TF_cofactor_list.xls", header= T, sep="\t",stringsAsFactors=FALSE)
 
 ma_plot <- ggplot(TF_list, aes(x=log2baseMean, y=log2FoldChange, color=Type)) + geom_point(size=0.5)+ scale_color_manual(values = c("no" = "gray", "TF" = "forestgreen", "cofactor" = "purple")) + xlim(0,16) + ylim(-10, 10) + theme(aspect.ratio=1) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), axis.line = element_line(colour = "black") + scale_y_continuous(breaks = seq(-10, 10, by = 2)))
 
