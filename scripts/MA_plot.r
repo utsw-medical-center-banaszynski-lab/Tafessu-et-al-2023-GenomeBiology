d3f3f98f51a64894973fbfbe8e30457321c3724b
AmanuelTafessu <49997327+AmanuelTafessu@users.noreply.github.com>
2021-11-16 14:15:30 -0600

    Update MA_plot.r

--- scripts/MA_plot.r
+++ scripts/MA_plot.r
@@ -1,17 +1,10 @@
-library(gdata)
-library(gplots)
-library(reshape)
-library(R.utils)
-library(dplyr)
 library(readxl)
 library(ggplot2)
-library(plyr)
-library(devtools)
-source_gist("524eade46135f6348140")
-library(ggpubr)
 library(ggrepel)
 
-TF_list <- read.table(file = "TF_cofactor_list.xls", header= T, sep="\t",stringsAsFactors=FALSE)
+
+#Read .xls file containing TF/cofactor ID, RNA-seq baseMean, log2 fold change, and column for labelling ("no", "TF" or "cofactor")
+TF_list <- read.table(file = "TFs_cofactors_log2baseMean_log2FC_label.xls", header= T, sep="\t",stringsAsFactors=FALSE)
 
 ma_plot <- ggplot(TF_list, aes(x=log2baseMean, y=log2FoldChange, color=Type)) + geom_point(size=0.5)+ scale_color_manual(values = c("no" = "gray", "TF" = "forestgreen", "cofactor" = "purple")) + xlim(0,16) + ylim(-10, 10) + theme(aspect.ratio=1) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), axis.line = element_line(colour = "black") + scale_y_continuous(breaks = seq(-10, 10, by = 2)))
 
